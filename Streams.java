a67e45e8846406ac847c6aef0637130dc81388cd
talundy <tlundy@calpoly.edu>
2023-03-06 13:04:39 -0800

    shit finally fucking works

--- Streams.java
+++ Streams.java
@@ -11,20 +11,34 @@ public class Streams {
     ){
         try{
             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            // create stream of input lines
             Stream<String> fileStream = Files.lines(Paths.get(filename));
-            fileStream.forEach(x -> {
-                if (x.length() == 0){
-                    return;
-                }
+            // convert to a Point stream, where we perform the necessary operations
+            Stream<Point> pointStream = fileStream
+                    .map(x -> {
+                        String[] s = x.split(",");
+                        return new Point(
+                                Double.parseDouble(s[0]),
+                                Double.parseDouble(s[1]),
+                                Double.parseDouble(s[2]));})
+                    .filter(pt -> !(pt.z > 2))
+                    .map(pt -> new Point(pt.x/2 - 150, pt.y/2 - 37, pt.z));
+            // map back to a String stream for printing
+            Stream<String> returnStream = pointStream
+                    .map(pt -> pt.x + ", " + pt.y + ", " + pt.z);
+            // write to output file
+            returnStream.forEach(str -> {
                 try {
-                    writer.write(x.toString());
+                    writer.write(str);
                     writer.write("\n");
                 } catch (IOException e) {
+                    System.out.println("Exception " + e + "thrown trying to write.");
                     throw new RuntimeException(e);
-                }
-            });
+                }});
+            // always close file!
             writer.close();
         } catch (IOException e) {
+            System.out.println("Exception " + e + "thrown trying to process file. ");
             throw new RuntimeException(e);
         }
 
